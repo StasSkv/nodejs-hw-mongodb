d8b922d88e98c5635ccee4a8deaa3b1d65849e3d
StasSkv <stas000123@gmail.com>
2025-06-24 11:16:43 +0300

    1-7ok

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-
-import contactsRouter from './routers/contacts.js';
-import welcomeRouter from './routers/home.js';
-
 import { logger } from './middlewares/logger.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -15,9 +13,9 @@ export const setupServer = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   app.use(logger);
-  app.use(welcomeRouter);
-  app.use(contactsRouter);
+  app.use(router);
   app.use(notFoundHandler);
   app.use(errorHandler);
   app.listen(PORT, () => {
